3d4e6efd45ea24b2764ac10c51f8acf06eabdf32
chulphan <chkim100617@gmail.com>
2019-04-24 12:17:33 +0900

    POST 한 결과가 테이블에 나오게 됨!!

--- functional_tests.py
+++ functional_tests.py
@@ -36,7 +36,7 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # 철환이는 "그램을 살거야" 라고 텍스트 박스에 입력한다. (철환이의 취미는 갖고 싶은게 있으면 막 지른다..(??))
-        inputbox.send_keys("Buy gram")
+        inputbox.send_keys("1: Buy gram")
 
         # 철환이가 엔터를 치면 그 페이지가 업데이트 되고 업데이트가 되고 난 페이지에는
         # "1: Buy gram" 이 to-do list 테이블 내의 하나의 아이템으로 생긴다.
@@ -45,18 +45,26 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy gram' for row in rows),
-            "테이블에 새로운 to-do item이 나타나지 않았습니다.."
-        )
+        self.assertIn('1: Buy gram', [row.text for row in rows])
 
         # 여전히 다른 아이템을 추가할 수 있는 텍스트 박스가 존재한다.
         # 철환이는 그램을 사고 윈도우를 깔 것이라고 엔터친다.
-        self.fail("Finish the test!!!")
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('2: Install Windows on gram!')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
         
         # 페이지가 또 업데이트 되면, 방금 전에 입력한 데이터도 같이 리스트에 보인다.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy gram', [row.text for row in rows])
+        self.assertIn(
+            '2: Install Windows on gram!',
+            [row.text for row in rows]
+        )
         #[...]
-
+        self.fail("Finish the test!!!")
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
