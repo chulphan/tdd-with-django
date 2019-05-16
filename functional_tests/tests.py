07b4642803336b647d6069645f786d9ca864a34d
chulphan <chkim100617@gmail.com>
2019-05-16 17:37:49 +0900

    virtualenv requirments에gunicorn을 추가함^^

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -17,6 +17,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server:
             self.live_server_url = 'http://' + staging_server
+        else:
+            self.live_server_url = 'http://' + 'localhost:8000'
         # self.browser = webdriver.Firefox()
 
     def tearDown(self):
@@ -31,7 +33,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 철환이는 쩌는 새로운 온라인 to-do app이 있다는 것을 들었다.
         # 그래서 그 홈페이지를 확인하러 갔다.
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # 철환이는 그 홈페이지의 제목과 헤더에 to-do lists로 명시되어있는 것을
         # 알아차렸다.
