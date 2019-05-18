6ff6f90f29cca4511e91a130d859dcfdbc6bd392
Ubuntu <ubuntu@ip-172-31-20-87.ap-northeast-2.compute.internal>
2019-05-18 06:00:57 +0000

    충돌 병합

--- tddtutorial/settings.py
+++ tddtutorial/settings.py
@@ -23,10 +23,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g5wxx*a*=ptvn)2f3b_!_j(6dxr%x-0n17i1&5&($&-1!=$)&j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+TEMPLATE_DEBUG = DEBUG
 
+ALLOWED_HOSTS = ['chulphan.me']
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
