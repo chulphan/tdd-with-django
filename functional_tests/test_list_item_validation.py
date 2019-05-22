2a147bb3fe6e97d63b19c1f18fc505b93d905788
chulphan <chkim100617@gmail.com>
2019-05-22 18:20:23 +0900

    모든 뷰에 form을 사용했고, 코드가 정상적으로 동작하도록 했다

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -37,3 +37,35 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy Pencil')
         self.wait_for_row_in_list_table('2: Write something')
         # self.fail('write me!')
+
+    def test_cannot_add_empty_list_items(self):
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
+        ))
+
+        self.get_item_input_box().send_keys('Buy Pencil')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
+
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy Pencil')
+
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: Buy Pencil')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
+        ))
+
+        self.get_item_input_box().send_keys('Write something')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
+
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy Pencil')
+        self.wait_for_row_in_list_table('2: Write something')
