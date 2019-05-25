f874be388679eb8ec6e8c0ce58964b5371b99bcc
chulphan <chkim100617@gmail.com>
2019-05-25 15:07:30 +0900

    front > 리스트 내 중복된 아이템 입력시 발생한 에러가 타이핑을 하면 없어지도록 처리

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,5 @@
-var initialize = function() {
+window.Tddtutorial = {};
+window.Tddtutorial.initialize = function() {
     $('input[name="text"]').on('keypress', function() {
         $('.has-error').hide();
     })
